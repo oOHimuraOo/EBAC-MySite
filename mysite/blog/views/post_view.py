0a9ed5183632ffe0d6e35884b946304729ad7921
Felipe Cerqueira <felipecer06@gmail.com>
2024-10-20 20:13:28 -0300

    pedido de socorro

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.views import generic
+
+# class PostView(generic.View):
+#     def get(self, request, *args, **kwargs):
+#         return HttpResponse('Hello World')
 from django.views import generic
 
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World! enviado via http response')
+from blog.models import Post
+
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
